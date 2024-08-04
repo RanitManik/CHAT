e392b1f0d7bc9d9f6f3d117b788a32828a31a129
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-08-04 15:10:59 +0530

    :bug: delay fixed to 300ms added extra padding for the Dashboard

--- src/components/block/Dashboard.component.jsx
+++ src/components/block/Dashboard.component.jsx
@@ -3,7 +3,7 @@ import NavigationComponent from "./Navigation.component.jsx";
 const DashboardComponent = () => {
   return (
     <main className="m-auto grid min-h-svh place-items-center duration-200 animate-in fade-in">
-      <div className="m-auto h-full max-h-[1000px] w-full max-w-[1900px] overflow-auto bg-background px-4 lg:border-2">
+      <div className="m-auto h-full max-h-[1000px] w-full max-w-[1900px] overflow-auto bg-background px-4 md:px-6 lg:border-2 lg:px-8">
         <NavigationComponent />
       </div>
     </main>
--- src/components/block/Error.component.jsx
+++ src/components/block/Error.component.jsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button.jsx";
 const ErrorComponent = ({ ERROR_DETAILS }) => {
   return (
     <main
-      className="grid min-h-svh place-items-center content-center gap-6 text-center"
+      className="grid min-h-svh place-items-center content-center gap-6 px-4 text-center"
       aria-live="assertive"
     >
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
