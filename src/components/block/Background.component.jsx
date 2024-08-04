354590b82e5ce71f1b2529652d90fc438dc2a789
RanitManik <138437760+RanitManik@users.noreply.github.com>
2024-08-04 13:43:34 +0530

    :bug: fix jittery transition in theme toggle :feature: added border in DashBoard component

--- src/components/block/Background.component.jsx
+++ src/components/block/Background.component.jsx
@@ -1,6 +1,6 @@
 const BackgroundComponent = ({ children }) => {
   return (
-    <div className="m-auto bg-background duration-200 animate-in fade-in">
+    <div className="m-auto duration-200 animate-in fade-in">
       <div className="mx-auto min-h-[100svh] max-w-[1900px] caret-primary">
         {children}
       </div>
--- src/components/block/Dashboard.component.jsx
+++ src/components/block/Dashboard.component.jsx
@@ -1,15 +1,12 @@
 import NavigationComponent from "./Navigation.component.jsx";
-import BackgroundComponent from "./Background.component.jsx";
 
 const DashboardComponent = () => {
   return (
-    <BackgroundComponent>
-      <div className="m-auto grid min-h-svh place-items-center">
-        <div className="m-auto h-full max-h-[1000px] w-full max-w-[1900px] overflow-auto bg-background px-4">
-          <NavigationComponent />
-        </div>
+    <main className="m-auto grid min-h-svh place-items-center duration-200 animate-in fade-in">
+      <div className="m-auto h-full max-h-[1000px] w-full max-w-[1900px] overflow-auto bg-background px-4 lg:border-2">
+        <NavigationComponent />
       </div>
-    </BackgroundComponent>
+    </main>
   );
 };
 
